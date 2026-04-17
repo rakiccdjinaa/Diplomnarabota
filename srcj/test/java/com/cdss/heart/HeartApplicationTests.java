54c4204491c0be4ff4494de2371ce8cbf9060a6c
Djina <130569130+rakiccdjinaa@users.noreply.github.com>
2026-04-17 11:41:17 +0200

    Add files via upload

---
+++ srcj/test/java/com/cdss/heart/HeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdss.heart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
