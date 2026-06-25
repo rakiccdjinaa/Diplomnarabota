1cd1f4ff5d52aab0dab0fee62b57f03727dbb5fe
Djina <130569130+rakiccdjinaa@users.noreply.github.com>
2026-06-25 12:41:30 +0200

    Update api.py

--- ml-part/api.py
+++ ml-part/api.py
@@ -1,66 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
-model = Pipeline([
-    ('scaler', StandardScaler()),
-    ('logreg', LogisticRegression(max_iter=1000))
-])
-
-model.fit(X, y)
-
+from config import Config
+from models.user import db
+from routes.auth_routes import auth_bp
+from routes.diagnosis_routes import diagnosis_bp
+from flask_jwt_extended import JWTManager
 app = Flask(__name__)
-CORS(app)  
 
+app.config.from_object(Config)
+app.config["JWT_SECRET_KEY"] = "cdss_project_2026_key"
 
-df = pd.read_csv("processed.cleveland.data", header=None)
+jwt = JWTManager(app)
+CORS(app)
 
-df.columns = [
-    "age", "sex", "cp", "trestbps", "chol",
-    "fbs", "restecg", "thalach", "exang",
-    "oldpeak", "slope", "ca", "thal", "target"
-]
+db.init_app(app)
 
-df = df.replace("?", None)
-df = df.dropna()
+app.register_blueprint(auth_bp)
+app.register_blueprint(diagnosis_bp)
 
+with app.app_context():
+    db.create_all()
 
-df["target"] = df["target"].apply(lambda x: 1 if int(x) > 0 else 0)
-
-
-X = df.drop("target", axis=1)
-y = df["target"]
-
-
-model = LogisticRegression(max_iter=1000)
-model.fit(X, y)
-
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    try:
-        data = request.json
-        input_data = pd.DataFrame([data])
-        
-        
-        input_data = input_data.fillna(0)
-        input_data = input_data.astype(float)
-        
-        
-        prediction = model.predict(input_data)[0]
-       
-        probability = model.predict_proba(input_data)[0][1]
-        
-        return jsonify({
-            "prediction": int(prediction),
-            "probability": round(float(probability) * 100, 2)
-        })
-    except Exception as e:
-        print(f"ERROR: {e}")
-        return jsonify({"error": str(e)}), 400
+print(app.url_map)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(
+        debug=True,
+        port=5000
+    )
